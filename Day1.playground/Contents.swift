0c9129f1caa135b588185908e4b32a0b0a1a4d78
Giovannini <giovanninibaarbosa@gmail.com>
2021-01-01 21:44:08 -0300

    Added day one playground.

---
+++ Day1.playground/Contents.swift
@@ -0,0 +1,49 @@
+// 100 Days of Swift - Day 1
+// Giovannini Barbosa
+
+import UIKit
+// Variables
+var str = "Hello, playground"
+str = "100 days of swift"
+
+
+// Strings and integers
+var age = 22
+var population = 8_000_000
+var name = "Giovannini"
+
+
+//Multi-line strings
+var str1 = """
+This goes
+over multiples
+lines
+"""
+
+var str2 = """
+This goes \
+over multiples \
+lines
+"""
+
+
+// Doubles and Booleans
+var pi = 3.1415
+var awesome = true
+
+
+// Strings interpolation
+var score = 85
+var str3 = "Your score was \(score)"
+var results = "Your actual age is: \(age)"
+
+
+// Constants
+let taylor = "swift"
+
+
+// Type annotations
+let album: String = "Reptilla"
+let year: Int = 2021
+let height: Double = 1.78
+let avaliable: Bool = true
